1e9027a7fe0a2d5c135f04a36ace386be8fe9d6b
fverdugo <fverdugo@cimne.upc.edu>
2019-08-27 12:29:14 +0200

    Deactivating tests for the moment

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 using Tutorials
 
-module t001_poisson
-using Test
-@testset "t001_poisson" begin include("../src/t001_poisson.jl") end
-end # module
+#module t001_poisson
+#using Test
+#@testset "t001_poisson" begin include("../src/t001_poisson.jl") end
+#end # module

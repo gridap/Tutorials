fd3b8adc1658aeade4a44d114576d8c0b2f912ce
fverdugo <fverdugo@cimne.upc.edu>
2019-08-27 12:43:04 +0200

    Deploying docs

--- docs/make.jl
+++ docs/make.jl
@@ -20,6 +20,7 @@ makedocs(
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+
+deploydocs(
+    repo = "github.com/gridap/Tutorials.git"
+)

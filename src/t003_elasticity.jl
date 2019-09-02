31f3c0a6f338126c22ebc39b033bf57dda4816e6
fverdugo <fverdugo@cimne.upc.edu>
2019-09-02 16:52:16 +0200

    Added API for multi-material cases. Now, its time to implement it!

--- src/t003_elasticity.jl
+++ src/t003_elasticity.jl
@@ -81,40 +81,8 @@ const E_steel = 200.0e9
 const ν_steel = 0.33
 const (λ_steel,μ_steel) = lame_parameters(E_steel,ν_steel)
 
-# ### First option: Split triangulation (Not yet implemented)
-#
-# Perhaps too lengthy
 
-@law σ_alu(x,ε) = λ_alu*tr(ε)*one(ε) + 2*μ_alu*ε
-@law σ_steel(x,ε) = λ_steel*tr(ε)*one(ε) + 2*μ_steel*ε
-
-trian_alu = Triangulation(model,"material_1")
-trian_steel = Triangulation(model,"material_2")
-
-quad_alu = CellQuadrature(trian_alu,order=2)
-quad_steel = CellQuadrature(trian_steel,order=2)
-
-a_alu(v,u) = inner( ε(v), σ_alu(ε(u)) )
-t_Ω_alu = LinearFETerm(a_alu,trian_alu,quad_alu)
-
-a_steel(v,u) = inner( ε(v), σ_steel(ε(u)) )
-t_Ω_steel = LinearFETerm(a_steel,trian_steel,quad_steel)
-
-op = LinearFEOperator(V0,U,t_Ω_alu,t_Ω_steel)
-uh = solve(op)
-
-uh_alu = restrict(uh,trian_alu)
-uh_steel = restrict(uh,trian_steel)
-
-writevtk(trian_alu,"results_alu", cellfields=
-  ["uh"=>uh_alu,"epsi"=>ε(uh_alu),"sigma"=>σ_alu(ε(uh_alu))])
-
-writevtk(trian_steel,"results_steel", cellfields=
-  ["uh"=>uh_steel,"epsi"=>ε(uh_steel),"sigma"=>σ_steel(ε(uh_steel))])
-
-# ### Second option: Pass mask to constitutive law (Not yet implemented)
-#
-# I think, I will implement this one
+# **Most of the following API is not yet implemented!**
 
 @law function σ_bimat(x,ε,mask)
   if mask

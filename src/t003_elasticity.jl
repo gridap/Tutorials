ed0e69b782dc670e0a5795f17495259b45e3a519
fverdugo <fverdugo@cimne.upc.edu>
2019-09-03 16:54:03 +0200

    More rewritting in tutorial 003

--- src/t003_elasticity.jl
+++ src/t003_elasticity.jl
@@ -99,12 +99,12 @@ U = TrialFESpace(V,[g1,g2]);
 
 # ### Constitutive law
 #
-# In this example, the definition of the terms  in the weak form requires more work that in previous tutorials, in particular, for the definition of the bilinear form.  In this case, the integrand of the bilinear form is defined as
+# Once the FE spaces are defined, the next step is to define the weak form.  In this example, the construction of the weak form requires more work than in previous tutorials since we need to account for the constitutive law that relates strain and stress. This becomes evident, when defining (the integrand of) the bilinear form of the problem:
 #
 
 a(v,u) = inner( ε(v), σ(ε(u)) )
 
-# Note that we have used the Gridap function `ε` (aka `symmetric_gradient`) to compute the symmetric gradient of the test and trial functions. On the other hand, `σ` is a function (to be defined below) that computes the stress tensor associated with `ε(u)`. In Gridap, function `σ` and other types of constitutive laws are defined by using the supplied macro `@law`:
+# The symmetric gradient of the test and trial functions are simply computed with the `ε` function (aka `symmetric_gradient`) provided by Gridap. However, the function `σ` that computes the stress tensor has to be defined ad-hoc for this example as we detail now.  The way function `σ` and other types of constitutive laws are defined  in Gridap is by using the supplied macro `@law`:
 
 const E = 70.0e9
 const ν = 0.33
@@ -114,20 +114,20 @@ const μ = E/(2*(1+ν))
 
 @law σ(x,ε) = λ*tr(ε)*one(ε) + 2*μ*ε
 
-# The macro `@law` is always placed before a function definition.  The arguments of the function annotated with the `@law` macro represent the values of different quantities at a generic integration point. The first argument always represents the coordinate of the integration point. The rest of the argument have arbitrary meaning. In this example, the second argument represents the strain tensor at an integration point, from which the stress tensor is computed. Note that we have used the function `tr` and function `one` to compute the stress tensor. We have used material parameters corresponding to aluminum.
+# Here are some rules that must be considered, when using this macro. The macro `@law` is always placed before a function definition.  The arguments of the function annotated with the `@law` macro represent the values of different quantities at a generic integration point. The first argument always represents the coordinate of the integration point. The remaining arguments have arbitrary meaning. In this example, the second argument represents the strain tensor, from which the stress tensor is to be computed. We have used the function `tr` (aka `trace`) and function `one` in the computation of the stress tensor, whereas we have used material parameters corresponding to aluminum.
 #
-# The macro `@law` automatically adds an extra method to the corresponding function. The generated method has always an argument less that the original function definition (i.e., the first argument is removed). You can easily check it with following line
+# The macro `@law` automatically adds an extra method to the annotated function. The generated method has always an argument less that the original function definition (i.e., the first argument is removed). You can easily check this with following line
 
 methods(σ)
 
 #md # If you run previous in a notebook, we will see
 #nb # Note
-# that function `σ` has indeed two methods: the one we have defined, which has 2 arguments, and another one with only a single argument that has been created by the macro. The new method can be used as `σ(ε(u))` where `u` is a trial function in the definition of a bilinear form (as done above), or where `u` is a `FEFunction` (we will use this for writing the stress tensor into the vtk file).
+# that function `σ` has indeed two methods: the one we have defined, which has 2 arguments, and another with only a single argument that has been created by the macro. The macro-generated method can be used as `σ(ε(u))` where `u` is a trial function in the definition of a bilinear form (as done above), or where `u` is a `FEFunction` (as we will use for writing the stress tensor into a vtk file).
 #
 #
 # ### Solution of the FE problem
 #
-# The remaining steps for solving the FE problem are as in previous tutorials. We build the integration mesh and quadrature for integrating in the volume
+# The remaining steps for solving the FE problem are essentially the same as in previous tutorials. We build the integration mesh and quadrature for integrating in the volume
 
 trian = Triangulation(model)
 quad = CellQuadrature(trian,order=2);
@@ -141,36 +141,37 @@ op = LinearFEOperator(V0,U,t_Ω);
 
 uh = solve(op);
 
-# Note that in the construction of the `LinearFEOperator` we have used a `LinearFETerm` instead of an `AffineFETerm` as it was done in previous tutorial. The `LinearFETerm` is a particular implementation of `FETerm` for terms that only contribute to the system matrix (and not to the right hand side vector). This is what we want in this example since the body forces are zero.
+# Note that in the construction of the `LinearFEOperator` we have used a `LinearFETerm` instead of an `AffineFETerm` as it was done in previous tutorial. The `LinearFETerm` is a particular implementation of `FETerm`, which only leads to contributions to the system matrix (and not to the right hand side vector). This is what we want in this example since the body forces are zero.
 #
 # Finally, we write the results to a file
 
 writevtk(trian,"results",cellfields=["uh"=>uh,"epsi"=>ε(uh),"sigma"=>σ(ε(uh))])
 
-# Note that we are also including the strain and stress tensors into the results file. Open it, to see the computed solution of the problem. You can clearly see that the surface  $\Gamma_{\rm B}$ is pulled in $x_1$-direction and that the solid deforms accordingly (in the figure below deformation magnified 40 times).
+# Note that we are also including the strain and stress tensors into the results file (indeed, we are using the generated constitutive law for computing the stress tensor associated with the solution). Open the results file and see the computed solution of the problem. It can be clearly observed that the surface  $\Gamma_{\rm B}$ is pulled in $x_1$-direction and that the solid deforms accordingly (in the figure below deformation magnified 40 times).
 #
 # ![](../assets/t003_elasticity/disp_ux_40.png)
 
 
 # ## Multi-material problems
 #
-# In practical applications it is often the case that a component is made of several materials with different material properties, and therefore, the underlying FE code has to be able to deal with this. We end this tutorial by extending previous code to deal with multi-material problems. Let us consider that the piece simulated before is now made of 2 different materials (see next figure). In particular, we assume that the volume depicted in dark green is made of aluminum, whereas the volume marked in purple is made of steel.
+# We end this tutorial by extending previous code to deal with multi-material problems. Let us assume that the piece simulated before is now made of 2 different materials (see next figure). In particular, we assume that the volume depicted in dark green is made of aluminum, whereas the volume marked in purple is made of steel.
 #
 # ![](../models/solid-mat.png)
 #
-# The first thing we need is that the two different material volumes are properly identified in the model. To check this, inspect the model with Paraview (by writing it to vtk format as done before). Note that the volume made of aluminum is identified as `"material_1"`, whereas the volume made of steel is identified as `"material_2"`.
+# The two different material volumes are properly identified in the model we have previously loaded. To check this, inspect the model with Paraview (by writing it to vtk format as done before). Note that the volume made of aluminum is identified as `"material_1"`, whereas the volume made of steel is identified as `"material_2"`.
 #
-# The following two lines build a vector, namely `tags`, whose length is the number of cells in the model and for each cell contains an integer that identifies the material of the cell. 
+# In order to build the constitutive law for the bi-material problem, we need a vector that contains information about the material each cell in the model is composed. This is achieved by these lines
 
 labels = FaceLabels(model)
 dimension = 3
 tags = first_tag_on_face(labels,dimension);
 
-# We use the following line in order to retrieve the integer value associated with `"material_1"`.
+#
+# Previous lines generate a vector, namely `tags`, whose length is the number of cells in the model and for each cell contains an integer that identifies the material of the cell.  This is almost what we need. We also need to know which is the integer value associated with each material. E.g., the integer value associated with `"material_1"` (i.e. aluminum) is retrieved as
 
 const alu_tag = tag_from_name(labels,"material_1")
 
-# That is, all cells whose corresponding value in the `tags` vector is `alu_tag` are made of aluminum, otherwise they are made of steel since there are only two materials in this example.
+# Now, we know that cells whose corresponding value in the `tags` vector is `alu_tag` are made of aluminum, otherwise they are made of steel (since there are only two materials in this example).
 #
 # At this point, we are ready to define the multi-material constitutive law. First, we define the material parameters for aluminum and steel respectively:
 
@@ -188,7 +189,7 @@ const E_steel = 200.0e9
 const ν_steel = 0.33
 const (λ_steel,μ_steel) = lame_parameters(E_steel,ν_steel)
 
-# Then, we define the function representing the constitutive law
+# Then, we define the function containing the constitutive law:
 
 @law function σ_bimat(x,ε,tag)
   if tag == alu_tag
@@ -198,11 +199,13 @@ const (λ_steel,μ_steel) = lame_parameters(E_steel,ν_steel)
   end
 end
 
-# Note that the last argument represents the integer value associated with a certain material. If the value corresponds to the one for aluminum, then we use the constitutive law for this material, otherwise, we use the law for steel. With the generated constitutive law, we can re-define the bilinear form of the problems:
+# Note that in this new version of the constitutive law, we have included a third argument that represents the integer value associated with a certain material. If the value corresponds to the one for aluminum (i.e., `tag == alu_tag`), then, we use the constitutive law for this material, otherwise, we use the law for steel.
+#
+# Since we have constructed a new constitutive law, we need to re-define the bilinear form of the problem:
 
 a(v,u) = inner( ε(v), σ_bimat(ε(u),tags) )
 
-# Note that we have passed the vector containing the material tags in the last argument of the function `σ_bimat`.
+# In previous line, pay attention in the usage of the new constitutive law `σ_bimat`. Note that we have passed the vector `tags` containing the material identifiers in the last argument of the function`.
 #
 # At this point, we can build the FE problem again and solve it
 
@@ -210,7 +213,7 @@ t_Ω = LinearFETerm(a,trian,quad)
 op = LinearFEOperator(V0,U,t_Ω)
 uh = solve(op);
 
-# One the solution is computed, we can store the results in a file for visualization. Note that, we are also including the stress tensor in the file (computed with the bi-material law).
+# Once the solution is computed, we can store the results in a file for visualization. Note that, we are including the stress tensor in the file (computed with the bi-material law).
 
 writevtk(trian,"results",cellfields=
   ["uh"=>uh,"epsi"=>ε(uh),"sigma"=>σ_bimat(ε(uh),tags)])

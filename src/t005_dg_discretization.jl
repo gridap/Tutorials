5b3d593afeb213a1cece38ab836105f73c373eb9
fverdugo <fverdugo@cimne.upc.edu>
2019-09-12 15:54:30 +0200

    More work on dg tutorial

--- src/t005_dg_discretization.jl
+++ src/t005_dg_discretization.jl
@@ -1,41 +1,61 @@
 
 using Gridap
+import Gridap: ∇
 
 u(x) = x[1] + x[2]
+∇u(x) = VectorValue(1.0,1.0)
+∇(::typeof(u)) = ∇u
 f(x) = 0.0
+g(x) = u(x)
 
-limits = (0.0, 1.0, 0.0, 1.0)
-model = CartesianDiscreteModel(domain=limits, partition=(4,4))
+L = 1.0
+limits = (0.0, L, 0.0, L)
+ncellx = 4
+model = CartesianDiscreteModel(domain=limits, partition=(ncellx,ncellx))
 
-order = 1
-V = DLagrangianFESpace(Float64,model,order)
+h = L / ncellx
 
-V0 = TestFESpace(V)
-U = TrialFESpace(V)
+γ = 10
+
+order = 3
+fespace = DLagrangianFESpace(Float64,model,order)
+
+V = TestFESpace(fespace)
+U = TrialFESpace(fespace)
 
 trian = Triangulation(model)
-quad = CellQuadrature(trian,order=2)
+quad = CellQuadrature(trian,order=2*order)
 
-btrian = BoundaryTriangulation(model,"boundary")
-bquad = CellQuadrature(btrian,order=2)
+btrian = BoundaryTriangulation(model)
+bquad = CellQuadrature(btrian,order=2*order)
+nb = NormalVector(btrian)
 
-strian = BoundaryTriangulation(model,"interior")
-squad = CellQuadrature(strian,order=2)
+strian = SkeletonTriangulation(model)
+squad = CellQuadrature(strian,order=2*order)
+ns = NormalVector(strian)
 
 a_Ω(v,u) = inner(∇(v), ∇(u))
 b_Ω(v) = inner(v,f)
 t_Ω = AffineFETerm(a_Ω,b_Ω,trian,quad)
 
-a_∂Ω(v,u) = inner(v, ∇(u)*n ) - inner(∇(v)*n, u) + (γ/h) * inner(v,u)
-b_∂Ω(v) = (γ/h) * inner(v,g) - inner(∇(v)*n, g)
-t_∂Ω = AffineFETerm(a_∂Ω,b_∂Ω,trian,quad)
+a_∂Ω(v,u) = (γ/h) * inner(v,u) - inner(v, ∇(u)*nb ) - inner(∇(v)*nb, u)
+b_∂Ω(v) = (γ/h) * inner(v,g) - inner(∇(v)*nb, g)
+t_∂Ω = AffineFETerm(a_∂Ω,b_∂Ω,btrian,bquad)
 
-a_Γ(v,u) = 
-  inner( jump(v*n), mean(∇(u)) ) -
-  inner( mean(∇(v)), jump(u*n) ) + 
-  (γ/h) * inner( jump(v*n), jump(u*n))
+a_Γ(v,u) = (γ/h) * inner( jump(v*ns), jump(u*ns))
+  - inner( jump(v*ns), mean(∇(u)) ) - inner( mean(∇(v)), jump(u*ns) ) 
+t_Γ = LinearFETerm(a_Γ,strian,squad)
 
-t_Γ = LinearFEOperator(a_Γ,strian,squad)
+op = LinearFEOperator(V,U,t_Ω,t_∂Ω,t_Γ)
 
+uh = solve(op)
 
+e = u - uh
 
+#writevtk(trian,"trian",cellfields=["uh"=>uh,"e"=>e])
+
+l2(u) = inner(u,u)
+h1(u) = a_Ω(u,u) + l2(u)
+
+el2 = sqrt(sum( integrate(l2(e),trian,quad) ))
+eh1 = sqrt(sum( integrate(h1(e),trian,quad) ))

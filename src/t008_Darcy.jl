5f058192796ab6f6c3dae43b616cc3be7fca2a78
Santiago Badia <santiago.badia@monash.edu>
2019-09-23 23:33:48 +1000

    Working on Darcy test

--- src/t008_Darcy.jl
+++ src/t008_Darcy.jl
@@ -7,78 +7,50 @@ using Test
 using Gridap
 import Gridap: ∇
 
-const T = VectorValue{2,Float64}
-
 # Construct the discrete model
-model = CartesianDiscreteModel(domain=(0.0,1.0,0.0,1.0), partition=(2,2))
+model = CartesianDiscreteModel(domain=(0.0,1.0,0.0,1.0), partition=(50,50))
 
-# Construct the FEspace 1
+# Construct the mixed FE spaces
 order = 2
-D = 2
-p = Polytope(fill(HEX_AXIS,D)...)
-reffe = RaviartThomasRefFE(p,order)
-
-grid = Grid(model,D)
-trian = Triangulation(grid)
-graph = GridGraph(model)
-labels = FaceLabels(model)
-diritags = [5,6]
-
-# @santiagobadia : Again, find a general constructor for all FE spaces
-fespace1 = ConformingFESpace(reffe,trian,graph,labels,diritags)
-
-# Construct the FEspace 2
-reffe = PDiscRefFE(Float64,D,order-1)
-fespace2 = DiscFESpace(reffe,model)
+V = FESpace( reffe=:RaviartThomas, conformity=:HDiv, valuetype = Float64, order=2, model=model, diritags = [5,6])
+_Q = FESpace( reffe=:PLagrangian, conformity=:L2, valuetype = Float64, order = 1, model = model)
 
 # Define test and trial
-V1 = TestFESpace(fespace1)
-V2 = TestFESpace(fespace2)
-V = [V1, V2]
+V_0 = TestFESpace(V)
+Q = TestFESpace(_Q) # Since no bc's the trial space is the same
+Y = [V_0, Q]
 
+const ν = 1000.0
 
+ue(x) = VectorValue(ν, 0.0)
+pe(x) = 1.0-x[1]
 
-ν = 1.0
-# @santiagobadia : It produces an error that should be fixed
-# u1(x) = ν*VectorValue(1.0,0.0)
-# By the way if we put (10,0.0) it does not work either
-u1(x) = VectorValue(1.0, 0.0)
-u2(x) = 1.0-x[1]
-U1 = TrialFESpace(fespace1,u1)
-U2 = TrialFESpace(fespace2)
-U = [U1, U2]
+V_g = TrialFESpace(V,ue)
+# Q_trial = TrialFESpace(Q)
+X = [V_g, Q]
+
 
 # Define integration mesh and quadrature for volume
 trian = Triangulation(model)
 quad = CellQuadrature(trian,order=2)
 
-# @santiagobadia : If I eliminate u in the interface it does not work.
-# It has full sense to define a law that only depens on the space corordinates
-
 const kinv_1 = TensorValue(1.0,0.0,0.0,1.0)
-const kinv_2 = TensorValue(100.0,90.0,90.0,100.0)
+const kinv_2 = TensorValue(1.0/ν,0.0,0.0,1.0)
+# const kinv_2 = TensorValue(100.0,90.0,90.0,100.0)
 
-#@law function σ(x,u)
-#  if ((abs(x[1]-0.5) <= 0.25) && (abs(x[2]-0.5) <= 0.25))
-#    return kinv_1*u
-#  else
-#    return kinv_2*u
-#  end
-#end
-@law σ(x,u) = kinv_1*u
+@law function σ(x,u)
+ if ((abs(x[1]-0.5) <= 0.1) && (abs(x[2]-0.5) <= 0.1))
+   return kinv_1*u
+ else
+   return kinv_2*u
+ end
+end
+#
+# @law σ(x,u) = kinv_1*u
 
-# @santiagobadia : If I use this law I get an error
-#@law σ(x,u) = kinv_1*u
-# a(v,u) =
-#   inner(v[1],σ(u[1])) - inner(div(v[1]),u[2]) + inner(v[2],div(u[1]))
-
-# So, I am using now
 a(v,u) =
    inner(v[1],σ(u[1])) - inner(div(v[1]),u[2]) + inner(v[2],div(u[1]))
-# b(v) = inner(v[1],b1) + inner(v[2],b2)
-# t_Ω = AffineFETerm(a,b,trian,quad)
 t_Ω = LinearFETerm(a,trian,quad)
-
 
 # Setup integration on Neumann boundary
 neumanntags = [7,]
@@ -86,50 +58,40 @@ btrian = BoundaryTriangulation(model,neumanntags)
 bquad = CellQuadrature(btrian,order=order*2)
 nb = NormalVector(btrian)
 
-# Integrand of the Neumann BC
-# gfun(x) = VectorValue(0.0,0.0)
-# gfun(x) = VectorValue(0.0,0.0)
-# g(v) = inner(v[1],gfun)
-
-# @santiagobadia : Doing this I get an error
 gfun(x) = -1.0
 g(v) = inner(v[1]*nb,gfun)
-
-
 # Define weak form terms
 t_ΓN = FESource(g,btrian,bquad)
 
 # Define Assembler
-assem = SparseMatrixAssembler(V,U)
+assem = SparseMatrixAssembler(Y,X)
 
 # Define FE problem
-op = LinearFEOperator(V,U,assem,t_Ω,t_ΓN)
+op = LinearFEOperator(Y,X,assem,t_Ω,t_ΓN)
 
 # Solve!
-uh = solve(op)
+xh = solve(op)
 
 # Define exact solution and error
-e1 = u1 - uh[1]
+# eu = ue - xh[1]
 
-e2 = u2 - uh[2]
+# ep = pe - xh[2]
 
-uh[1].free_dofs
-
-# Define norms to measure the error
-l2(u) = inner(u,u)
 # hdiv(u) = inner(div(u),div(u)) + l2(u)
 
+# e1l2 = sqrt(sum( integrate(l2(u1),trian,quad) ))
 # Compute errors
-e1l2 = sqrt(sum( integrate(l2(e1),trian,quad) ))
+# e1l2 = sqrt(sum( integrate(l2(eu),trian,quad) ))
 # e1h1 = sqrt(sum( integrate(hdiv(e1),trian,quad) ))
-uh[1].free_dofs
-e2l2 = sqrt(sum( integrate(l2(e2),trian,quad) ))
+# e2l2 = sqrt(sum( integrate(l2(ep),trian,quad) ))
 
-@test e1l2 < 1.e-8
+# xh[1].free_dofs
+
+# @test e1l2 < 1.e-8
 # @test e1hdiv < 1.e-8
 
-@test e2l2 < 1.e-8
+# @test e2l2 < 1.e-8
 
-writevtk(trian,"darcyresults",cellfields=["uh"=>uh[1],"ph"=>uh[2]])
+writevtk(trian,"darcyresults",cellfields=["uh"=>xh[1],"ph"=>xh[2]])
 ##
 end # module

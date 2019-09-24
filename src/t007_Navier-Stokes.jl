f572ef1cd1556cf7a6e055f6b48e755e04dffa42
fverdugo <fverdugo@cimne.upc.edu>
2019-09-24 15:50:16 +0200

    Some clean up in Navier-Stokes tutorial

--- src/t007_Navier-Stokes.jl
+++ src/t007_Navier-Stokes.jl
@@ -9,54 +9,101 @@ using Gridap
 import Gridap: ∇
 using LineSearches: BackTracking
 
-# The first part is identical to Stokes driver
 D = 2
 n = 100
 model = CartesianDiscreteModel(domain=(0.0,1.0,0.0,1.0), partition=(n,n))
-
+labels = FaceLabels(model)
+add_tag_from_tags!(labels,"diri0",[1,2,3,4,5,7,8])
+add_tag_from_tags!(labels,"diri1",[6,])
 
 order = 2
-const T = VectorValue{2,Float64}
-diritags = [1,2,3,4,5,6,7,8]
-fespace1 = CLagrangianFESpace(T,model,order,diritags)
-reffe = PDiscRefFE(Float64,D,order-1)
-_fespace2 = DiscFESpace(reffe,model)
+const T = VectorValue{D,Float64}
+diritags = ["diri0","diri1"]
+
+fespace1 = FESpace(
+  reffe=:Lagrangian,
+  conformity=:H1,
+  valuetype=T,
+  model=model,
+  labels=labels,
+  order=order,
+  diritags=diritags)
+
+#reffe = PDiscRefFE(Float64,D,order-1)
+#_fespace2 = DiscFESpace(reffe,model)
+#fespace2 = ConstrainedFESpace(_fespace2,fixeddofs)
+
+_fespace2 = FESpace(
+  reffe=:PLagrangian,
+  conformity=:L2,
+  valuetype=Float64,
+  model=model,
+  order=order-1,
+  constraint = :zeromean)
+
 fixeddofs = [1,]
-fespace2 = ConstrainedFESpace(_fespace2,fixeddofs)
-V1 = TestFESpace(fespace1)
-V2 = TestFESpace(fespace2)
-V = [V1, V2]
-uD_1(x) = VectorValue(0.0,0.0)
-uD_2(x) = VectorValue(1.0,0.0)
-uD = [ (i == 6) ? uD_2 : uD_1 for i = 1:8 ]
-U1 = TrialFESpace(fespace1,uD)
-U2 = TrialFESpace(fespace2)
-U = [U1, U2]
+#fespace2 = ConstrainedFESpace(_fespace2,fixeddofs)
+fespace2 = _fespace2
+
+V = TestFESpace(fespace1)
+Q = TestFESpace(fespace2)
+Y = [V, Q]
+
+uD0(x) = VectorValue(0.0,0.0)
+uD1(x) = VectorValue(1.0,0.0)
+U = TrialFESpace(fespace1,[uD0,uD1])
+P = TrialFESpace(fespace2)
+X = [U, P]
+
 trian = Triangulation(model)
 quad = CellQuadrature(trian,order=(order-1)*2)
 
-# Reynolds number
-const Re = 1.0
-# santiagobadia: This is the way that I see I can implement the convection term with the
-# current machinery
+const Re = 10.0
 @law conv(x,u,∇u) = Re*(∇u')*u
 @law dconv(x,du,∇du,u,∇u) = conv(x,u,∇du)+conv(x,du,∇u)
 
-# @santiagobadia : When the driver will work, I will do what we have said,
-# putting this in FieldValues module
-# (*)(u::VectorValue,::typeof(gradient)) = (∇du) -> ugrad(u,∇du)
-# function ugrad(u::VectorValue,∇du::VectorValue)
-# VectorValue(   (u.array)' *  ∇du.array   )
-# end
-
 # Terms in the volume
-a(v,u) = inner(∇(v[1]),∇(u[1])) - inner(div(v[1]),u[2]) + inner(v[2],div(u[1]))
+function a(y,x)
+  u, p = x
+  v, q = y
+  inner(∇(v),∇(u)) - inner(div(v),p) + inner(q,div(u))
+end
+
 c(v,u) = inner(v,conv(u,∇(u)))
 dc(v,du,u) = inner(v,dconv(du,∇(du),u,∇(u)))
-res(u,v) = a(v,u) + c(v[1],u[1])
-jac(u,v,du) = a(v,du) + dc(v[1],du[1],u[1])
+
+function res(x,y)
+  u, p = x
+  v, q = y
+  a(y,x) + c(v,u)
+end
+
+function jac(x,y,dx)
+  u, p = x
+  v, q = y
+  du, dp = dx
+  a(y,dx)+ dc(v,du,u)
+end
+
 t_Ω = NonLinearFETerm(res,jac,trian,quad)
-op = NonLinearFEOperator(V,U,t_Ω)
+op = NonLinearFEOperator(Y,X,t_Ω)
+
+#using Gridap.FEOperators: NonLinearOpFromFEOp
+#algop = NonLinearOpFromFEOp(op)
+#
+#X = MultiFESpace(X)
+#u = rand(num_free_dofs(X))
+#du = rand(num_free_dofs(X))
+#d = 0.000001
+#
+#e = residual(algop,u+d*du) - ( residual(algop,u) + d*jacobian(algop,u)*du )
+#
+#@show maximum(abs.(e))
+#
+#kk
+
+
+
 
 nls = JuliaNLSolver(
   show_trace=true,

8abbea3c9b15b2e26d1ba75d73d1d1a41bd36e72
Santiago Badia <santiago.badia@monash.edu>
2019-09-25 16:41:00 +1000

    Merge branch 'master' of github.com:gridap/Tutorials

--- src/t008_Darcy.jl
+++ src/t008_Darcy.jl
@@ -7,104 +7,75 @@ using Test
 using Gridap
 import Gridap: ∇
 
-# Construct the discrete model
-model = CartesianDiscreteModel(domain=(0.0,1.0,0.0,1.0), partition=(6,6))
+u(x) = VectorValue(0.0,0.0)
+∇u(x) = TensorValue(0.0,0.0,0.0,0.0)
+∇(::typeof(u)) = ∇u
 
-# Construct the mixed FE spaces
+p(x) = x[1]
+∇p(x) = VectorValue(1.0,0.0)
+∇(::typeof(p)) = ∇p
+
+model = CartesianDiscreteModel(domain=(0.0,1.0,0.0,1.0), partition=(50,50))
+
 order = 2
 V = FESpace( reffe=:RaviartThomas, conformity=:HDiv, order=2, model=model, diritags = [5,6])
-_Q = FESpace( reffe=:PLagrangian, conformity=:L2, valuetype = Float64, order = 1, model = model)
+_Q = FESpace( reffe=:QLagrangian, conformity=:L2, valuetype = Float64, order = 1,
+                    model = model)
 
-# Define test and trial
 V_0 = TestFESpace(V)
-Q = TestFESpace(_Q) # Since no bc's the trial space is the same
+Q = TestFESpace(_Q)
 Y = [V_0, Q]
 
-const ν = 1.0
-
-ue(x) = VectorValue(ν, 0.0)
-pe(x) = 1.0-x[1]
-
-V_g = TrialFESpace(V,ue)
-# Q_trial = TrialFESpace(Q)
+V_g = TrialFESpace(V,u)
 X = [V_g, Q]
 
-
-# Define integration mesh and quadrature for volume
 trian = Triangulation(model)
 quad = CellQuadrature(trian,order=2)
 
+const ν = 100.0
 const kinv_1 = TensorValue(1.0,0.0,0.0,1.0)
 const kinv_2 = TensorValue(1.0/ν,0.0,0.0,1.0)
-# const kinv_2 = TensorValue(100.0,90.0,90.0,100.0)
 
-# @law function σ(x,u)
-#  if ((abs(x[1]-0.5) <= 0.1) && (abs(x[2]-0.5) <= 0.1))
-#    return kinv_1*u
-#  else
-#    return kinv_2*u
-#  end
-# end
-#
-@law σ(x,u) = kinv_1*u
+@law function σ(x,u)
+ if ((abs(x[1]-0.5) <= 0.1) && (abs(x[2]-0.5) <= 0.1))
+   return kinv_1*u
+ else
+   return kinv_2*u
+ end
+end
 
-a(v,u) =
-   inner(v[1],u[1]) - inner(div(v[1]),u[2]) + inner(v[2],div(u[1]))
+function a(y,x)
+  v, q = y
+  u, p = x
+  inner(v,σ(u)) - inner(div(v),p) + inner(q,div(u))
+end
+
 t_Ω = LinearFETerm(a,trian,quad)
 
-# Setup integration on Neumann boundary
-neumanntags = [7,]
+neumanntags = [7,8]
 btrian = BoundaryTriangulation(model,neumanntags)
 bquad = CellQuadrature(btrian,order=order*2)
 nb = NormalVector(btrian)
 
-gfun(x) = -1.0
-g(v) = inner(v[1]*nb,gfun)
-# Define weak form terms
-t_ΓN = FESource(g,btrian,bquad)
+function b_Γ(y)
+  v, q = y
+  -inner(v*nb,p)
+end
 
-# Define Assembler
-assem = SparseMatrixAssembler(Y,X)
+t_Γ = FESource(b_Γ,btrian,bquad)
 
-# Define FE problem
-op = LinearFEOperator(Y,X,assem,t_Ω,t_ΓN)
+op = LinearFEOperator(Y,X,t_Ω,t_Γ)
 
-# Solve!
-xh = solve(op)
+uh, ph = solve(op)
 
-
-# Define exact solution and error
-eu = ue - xh[1]
-
-ep = pe - xh[2]
+e1 = u - uh
+e2 = p - ph
 
 l2(u) = inner(u,u)
+hdiv(u) = inner(div(u),div(u)) + l2(u)
 
-# res(u) = a(u,u)
-# e1l2 = sqrt(sum( integrate(res(xh),trian,quad) ))
+writevtk(trian,"/home/santiago/github-repos/Gridap/tmp/darcyresults",
+         cellfields=["uh"=>uh,"ph"=>ph])
 
-# e1l2 = sqrt(sum( integrate(g(xh),btrian,bquad) ))
-# hdiv(u) = inner(div(u),div(u)) + l2(u)
-
-# e1l2 = sqrt(sum( integrate(l2(u1),trian,quad) ))
-# Compute errors
-e1l2 = sqrt(sum( integrate(l2(eu),trian,quad) ))
-# e1h1 = sqrt(sum( integrate(hdiv(e1),trian,quad) ))
-e2l2 = sqrt(sum( integrate(l2(ep),trian,quad) ))
-
-# xh[1].free_dofs
-@test e1l2 < 1.e-8
-# @test e1hdiv < 1.e-8
-
-@test e2l2 < 1.e-8
-
-# f2(x) = VectorValue(x[1],-x[2])
-# uh = interpolate(V,f2)
-# f3(x) = x[1]+x[2]
-# ph = interpolate(Q,f3)
-# xh = [uh,ph]
-
-
-writevtk(trian,"/home/santiago/github-repos/Gridap/tmp/darcyresults",cellfields=["uh"=>xh[1],"ph"=>xh[2]])
 ##
 end # module

80c2bc4318fba3c0f3cf0c46b64a2fa713ce71ca
fverdugo <fverdugo@cimne.upc.edu>
2019-10-01 12:54:30 +0200

    adding "under construction" to hyperelasticity tutorial

--- src/t004_hyperelasticity.jl
+++ src/t004_hyperelasticity.jl
@@ -2,6 +2,11 @@
 #
 #md # [![](https://mybinder.org/badge_logo.svg)](@__BINDER_ROOT_URL__/notebooks/t004_hyperelasticity.ipynb)
 #md # [![](https://img.shields.io/badge/show-nbviewer-579ACA.svg)](@__NBVIEWER_ROOT_URL__/notebooks/t004_hyperelasticity.ipynb)
+#
+#md # !!! note
+#
+#     This tutorial is under construction, but the code below is already functional.
+#
 # 
 # ## Problem statement
 

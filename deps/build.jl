44e9048d985e89dd50371affd463aa987d8e06df
fverdugo <fverdugo@cimne.upc.edu>
2019-10-02 17:28:57 +0200

    Better names in the generated notebooks

--- deps/build.jl
+++ deps/build.jl
@@ -6,17 +6,17 @@ notebooks_dir = joinpath(@__DIR__,"..","notebooks")
 files = []
 
 files = [
-  "t001_poisson",
-  "t002_validation",
-  "t003_elasticity", 
-  "t0041_p_laplacian", 
-  "t004_hyperelasticity",
-  "t005_dg_discretization",
-  "t007_darcy",
-  "t008_inc_navier_stokes"]
+  "t001_poisson"=>"t001_poisson",
+  "t002_validation"=>"t002_code_validation",
+  "t003_elasticity"=>"t003_linear_elasticity", 
+  "t0041_p_laplacian"=>"t004_p_laplacian", 
+  "t004_hyperelasticity"=>"t005_hyperelasticity",
+  "t005_dg_discretization"=> "t006_poisson_with_dg",
+  "t007_darcy"=>"t007_darcy_with_rt",
+  "t008_inc_navier_stokes"=>"t008_inc_navier_stokes"]
 
 Sys.rm(notebooks_dir;recursive=true,force=true)
-for file in files
+for (file,name) in files
   file_jl = file*".jl"
-  Literate.notebook(joinpath(repo_src,file_jl), notebooks_dir; documenter=false, execute=false)
+  Literate.notebook(joinpath(repo_src,file_jl), notebooks_dir; name=name, documenter=false, execute=false)
 end

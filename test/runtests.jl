6d116e125ce237aa4d6f836b65318acd42caa567
victorsndvg <victorsv@gmail.com>
2020-03-05 16:39:54 +0100

    surround tests in a let block to try to bound test scopes

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,7 @@ using Tutorials
 using Test
 
 for (title,filename) in Tutorials.files
-    @testset "$title" begin include(joinpath("../src", filename)) end
+    let
+        @testset "$title" begin include(joinpath("../src", filename)) end
+    end
 end # module

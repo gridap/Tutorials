bbce0cc43ace6891d0485329e238665b052768d3
Oriol Colomes <oriol.colomes@gmail.com>
2020-11-19 21:21:54 +0100

    Updated Navier-Stokes tutorial

--- src/inc_navier_stokes.jl
+++ src/inc_navier_stokes.jl
@@ -1,9 +1,9 @@
 # In this tutorial, we will learn
 #  - How to solve nonlinear multi-field PDEs in Gridap
 #  - How to build FE spaces whose functions have zero mean value
-# 
+#
 # ## Problem statement
-# 
+#
 # The goal of this last tutorial is to solve a nonlinear multi-field PDE. As a model problem, we consider a well known benchmark in computational fluid dynamics, the lid-driven cavity for the incompressible Navier-Stokes equations. Formally, the PDE we want to solve is: find the velocity vector $u$ and the pressure $p$ such that
 #
 # ```math
@@ -21,7 +21,7 @@
 # ```math
 # \int_\Omega q \ {\rm d}\Omega = 0.
 # ```
-# 
+#
 # ## Numerical Scheme
 #
 # In order to approximate this problem we chose a formulation based on inf-sub stable $Q_k/P_{k-1}$ elements with continuous velocities and discontinuous pressures (see, e.g., [1] for specific details). The interpolation spaces are defined as follows.  The velocity interpolation space is
@@ -38,14 +38,14 @@
 # \end{aligned}
 # ```
 # where $P_{k-1}(T)$ is the polynomial space of multi-variate polynomials in $T$ of degree less or equal to $k-1$. Note that functions in $Q_0$ are strongly constrained to have zero mean value. This is achieved in the code by removing one degree of freedom from the (unconstrained) interpolation space $Q$ and  adding a constant to the computed pressure so that the resulting function has zero mean value.
-# 
+#
 # The weak form associated to these interpolation spaces reads: find $(u,p)\in U_g \times Q_0$ such that $[r(u,p)](v,q)=0$ for all $(v,q)\in V_0 \times Q_0$
 # where $U_g$ and $V_0$ are the set of functions in $V$ fulfilling the Dirichlet boundary condition $g$ and $0$  on $\partial\Omega$ respectively. The weak residual $r$ evaluated at a given pair $(u,p)$ is the linear form defined as
 #
 # ```math
 # [r(u,p)](v,q) \doteq a((u,p),(v,q))+ [c(u)](v),
 # ```
-# with 
+# with
 # ```math
 # \begin{aligned}
 # a((u,p),(v,q)) &\doteq \int_{\Omega} \nabla v \cdot \nabla u \ {\rm d}\Omega - \int_{\Omega} (\nabla\cdot v) \ p \ {\rm d}\Omega + \int_{\Omega} q \ (\nabla \cdot u) \ {\rm d}\Omega,\\
@@ -53,7 +53,7 @@
 # \end{aligned}
 # ```
 # Note that the bilinear form $a$ is associated with the linear part of the PDE, whereas $c$ is the contribution to the residual resulting from the convective term.
-# 
+#
 # In order to solve this nonlinear weak equation with a Newton-Raphson method, one needs to compute the Jacobian associated with the residual $r$. In this case, the Jacobian $j$ evaluated at a pair $(u,p)$ is the bilinear form defined as
 #
 # ```math
@@ -61,12 +61,12 @@
 # ```
 # where ${\rm d}c$ results from the linearization of the convective term, namely
 # ```math
-# [{\rm d}c(u)](\delta u,v) \doteq \int_{\Omega} v \cdot \left( (u\cdot\nabla)\ \delta u \right) \ {\rm d}\Omega + \int_{\Omega} v \cdot \left( (\delta u\cdot\nabla)\ u \right)  \ {\rm d}\Omega. 
+# [{\rm d}c(u)](\delta u,v) \doteq \int_{\Omega} v \cdot \left( (u\cdot\nabla)\ \delta u \right) \ {\rm d}\Omega + \int_{\Omega} v \cdot \left( (\delta u\cdot\nabla)\ u \right)  \ {\rm d}\Omega.
 # ```
 # The implementation of this numerical scheme is done in Gridap by combining the concepts previously seen for single-field nonlinear PDEs  and linear multi-field problems.
-# 
+#
 # ## Discrete model
-# 
+#
 # We start with the discretization of the computational domain. We consider a $100\times100$ Cartesian mesh of the unit square.
 
 using Gridap
@@ -87,17 +87,15 @@ add_tag_from_tags!(labels,"diri0",[1,2,3,4,5,7,8])
 
 D = 2
 order = 2
-V = TestFESpace(
-  reffe=:Lagrangian, conformity=:H1, valuetype=VectorValue{D,Float64},
-  model=model, labels=labels, order=order, dirichlet_tags=["diri0","diri1"])
+reffeᵤ = ReferenceFE(:Lagrangian,VectorValue{2,Float64},order)
+V = TestFESpace(model,reffeᵤ,conformity=:H1,labels=labels,dirichlet_tags=["diri0","diri1"])
 
 # The interpolation space for the pressure is built as follows
 
-Q = TestFESpace(
-  reffe=:PLagrangian, conformity=:L2, valuetype=Float64,
-  model=model, order=order-1, constraint=:zeromean)
+reffeₚ = ReferenceFE(:Lagrangian,Float64,order-1;space=:P)
+Q = TestFESpace(model,reffeₚ,conformity=:L2,constraint=:zeromean)
 
-# With the options `reffe=:PLagrangian`, `valuetype=Float64`, and `order=order-1`, we select the local polynomial space $P_{k-1}(T)$ on the cells $T\in\mathcal{T}$. With the symbol `:PLagrangian` we specifically chose a local Lagrangian interpolation of type "P". Using `:Lagrangian`, would lead to a local Lagrangian of type "Q" since this is the default for quadrilateral or hexahedral elements. On the other hand, `constraint=:zeromean` leads to a FE space, whose functions are constrained to have mean value equal to zero, which is just what we need for the pressure space. With these objects, we build the trial multi-field FE spaces
+# With the options `:Lagrangian`, `space=:P`, `valuetype=Float64`, and `order=order-1`, we select the local polynomial space $P_{k-1}(T)$ on the cells $T\in\mathcal{T}$. With the symbol `space=:P` we specifically chose a local Lagrangian interpolation of type "P". Without using `space=:P`, would lead to a local Lagrangian of type "Q" since this is the default for quadrilateral or hexahedral elements. On the other hand, `constraint=:zeromean` leads to a FE space, whose functions are constrained to have mean value equal to zero, which is just what we need for the pressure space. With these objects, we build the trial multi-field FE spaces
 
 uD0 = VectorValue(0,0)
 uD1 = VectorValue(1,0)
@@ -107,46 +105,39 @@ P = TrialFESpace(Q)
 Y = MultiFieldFESpace([V, Q])
 X = MultiFieldFESpace([U, P])
 
+# ## Triangulation and integration quadrature
+#
+# From the discrete model we can define the triangulation and integration measure
+
+degree = order
+Ωₕ = Triangulation(model)
+dΩ = LebesgueMeasure(Ωₕ,degree)
+
+
 # ## Nonlinear weak form
 #
 # The different terms of the nonlinear weak form for this example are defined following an approach similar to the one discussed for the $p$-Laplacian equation, but this time using the notation for multi-field problems.
 
 const Re = 10.0
-@law conv(u,∇u) = Re*(∇u')⋅u
-@law dconv(du,∇du,u,∇u) = conv(u,∇du)+conv(du,∇u)
+conv(u,∇u) = Re*(∇u')⋅u
+dconv(du,∇du,u,∇u) = conv(u,∇du)+conv(du,∇u)
 
-function a(x,y)
-  u, p = x
-  v, q = y
-  ∇(v)⊙∇(u) - (∇⋅v)*p + q*(∇⋅u)
-end
+# The bilinear form reads
+a((u,p),(v,q)) = ∫( ∇(v)⊙∇(u) - (∇⋅v)*p + q*(∇⋅u) )*dΩ
 
-c(u,v) = v⊙conv(u,∇(u))
-dc(u,du,v) = v⊙dconv(du,∇(du),u,∇(u))
+# The nonlinear term and its Jacobian are given by
+c(u,v) = ∫( v⊙(conv∘(u,∇(u))) )*dΩ
+dc(u,du,v) = ∫( v⊙(dconv∘(du,∇(du),u,∇(u))) )*dΩ
 
-function res(x,y)
-  u, p = x
-  v, q = y
-  a(x,y) + c(u,v)
-end
-
-function jac(x,dx,y)
-  u, p = x
-  v, q = y
-  du, dp = dx
-  a(dx,y)+ dc(u,du,v)
-end
+# Finally, the Navier-Stokes weak form residual and Jacobian can be defined as
+res((u,p),(v,q)) = a((u,p),(v,q)) + c(u,v)
+jac((u,p),(du,dp),(v,q)) = a((du,dp),(v,q)) + dc(u,du,v)
 
 # With the functions `res`, and `jac` representing the weak residual and the Jacobian, we build the nonlinear FE problem:
-
-trian = Triangulation(model)
-degree = (order-1)*2
-quad = CellQuadrature(trian,degree)
-t_Ω = FETerm(res,jac,trian,quad)
-op = FEOperator(X,Y,t_Ω)
+op = FEOperator(res,jac,X,Y)
 
 # ## Nonlinear solver phase
-# 
+#
 # To finally solve the problem, we consider the same nonlinear solver as previously considered for the  $p$-Laplacian equation.
 
 using LineSearches: BackTracking
@@ -160,10 +151,10 @@ uh, ph = solve(solver,op)
 
 # Finally, we write the results for visualization (see next figure).
 
-writevtk(trian,"ins-results",cellfields=["uh"=>uh,"ph"=>ph])
+writevtk(Ωₕ,"ins-results",cellfields=["uh"=>uh,"ph"=>ph])
 
 # ![](../assets/inc_navier_stokes/ins_solution.png)
-# 
+#
 #  ## References
-# 
+#
 #  [1] H. C. Elman, D. J. Silvester, and A. J. Wathen. *Finite elements and fast iterative solvers: with applications in incompressible fluid dynamics*. Oxford University Press, 2005.

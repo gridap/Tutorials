9e0d94fc5f3b76b604ae500ce2ffe3afc4a6550c
principe <principe@cimne.upc.edu>
2021-02-15 12:57:50 +0100

    Changed exact solution in validation test and fixed integration order

--- src/validation.jl
+++ src/validation.jl
@@ -144,10 +144,10 @@ tol = 1.e-10
 # We end up this tutorial by performing a convergence test, where we are going to use all the new concepts we have learned.  We will consider a manufactured solution that does not belong to the FE interpolation space. In this test, we expect to see the optimal convergence order of the FE discretization.
 
 # Here, we define the manufactured functions
-const k = 2*pi
-u(x) = sin(k*x[1]) * x[2]
-∇u(x) = VectorValue(k*cos(k*x[1])*x[2], sin(k*x[1]))
-f(x) = (k^2)*sin(k*x[1])*x[2]
+p = 3
+u(x) = x[1]^p+x[2]^p
+∇u(x) = VectorValue(p*x[1]^(p-1),p*x[2]^(p-1))
+f(x) = -p*(p-1)*(x[1]^(p-2)+x[2]^(p-2))
 
 # Since we have redefined the valiables `u`, `∇u`, and `f`, we need to execute these lines again
 
@@ -156,17 +156,17 @@ b(v) = ∫( v*f )*dΩ
 
 # In order to perform the convergence test, we write in a function all the code needed to perform a single computation and measure its error. The input of this function is the number of cells in each direction and the interpolation order. The output is the computed $L^2$ and $H^1$ error norms.
 
-function run(n,order)
+function run(n,k)
 
   domain = (0,1,0,1)
   partition = (n,n)
   model = CartesianDiscreteModel(domain,partition)
 
-  reffe = ReferenceFE(lagrangian,Float64,order)
+  reffe = ReferenceFE(lagrangian,Float64,k)
   V0 = TestFESpace(model,reffe,conformity=:H1,dirichlet_tags="boundary")
   U = TrialFESpace(V0,u)
 
-  degree = 2*(order-1)
+  degree = 2*p
   Ω = Triangulation(model)
   dΩ = Measure(Ω,degree)
 
@@ -188,7 +188,7 @@ end
 
 # The following function does the convergence test. It takes a vector of integers (representing the number of cells per direction in each computation) plus the interpolation order. It returns the $L^2$ and $H^1$ error norms for each computation as well as the corresponding cell size.
 
-function conv_test(ns,order)
+function conv_test(ns,k)
 
   el2s = Float64[]
   eh1s = Float64[]
@@ -196,7 +196,7 @@ function conv_test(ns,order)
 
   for n in ns
 
-    el2, eh1 = run(n,order)
+    el2, eh1 = run(n,k)
     h = 1.0/n
 
     push!(el2s,el2)
@@ -209,17 +209,18 @@ function conv_test(ns,order)
 
 end
 
-# We are ready to perform the test! We consider several mesh sizes and interpolation order equal to 2.
+# We are ready to perform the test! We consider several mesh sizes and interpolation order $k=1$ and $k=2$ (for $k=3$ the error will be close to machine precision, as before, except if you change the value of $p$ above).
 
-el2s, eh1s, hs = conv_test([8,16,32,64,128],2);
+el2s1, eh1s1, hs = conv_test([8,16,32,64,128],1);
+el2s2, eh1s2, hs = conv_test([8,16,32,64,128],2);
 
 # With the generated data, we do the classical convergence plot.
 
 using Plots
 
-plot(hs,[el2s eh1s],
+plot(hs,[el2s1 eh1s1 el2s2 eh1s2],
     xaxis=:log, yaxis=:log,
-    label=["L2" "H1"],
+    label=["L2 k=1" "H1 k=1" "L2 k=2" "H1 k=2"],
     shape=:auto,
     xlabel="h",ylabel="error norm")
 
@@ -229,7 +230,7 @@ plot(hs,[el2s eh1s],
 #md # ![](../assets/validation/conv.png)
 #
 #
-# The generated curves make sense. It is observed that the convergence of the $H^1$ error is slower that $L^2$ one. However, in order to be more conclusive, we need to compute the slope of these lines. It can be done with this little function that internally uses a linear regression.
+# The generated curves make sense. For a given interpolation order it is observed that the convergence of the $H^1$ error is slower that $L^2$ one whereas increasing the order makes convergence faster both in $L^2$ and in $H^1$. However, in order to be more conclusive, we need to compute the slope of these lines. It can be done with this little function that internally uses a linear regression.
 
 function slope(hs,errors)
   x = log10.(hs)
@@ -238,16 +239,18 @@ function slope(hs,errors)
   linreg[2]
 end
 
-# The slope for the $L^2$ error norm is computed as
+# The slopes for the $L^2$ error norm is computed as
 
-slope(hs,el2s)
+slope(hs,el2s1)
+slope(hs,el2s2)
 
-# and for the $H^1$ error norm
+# and for the $H^1$ error norm as
 
-slope(hs,eh1s)
+slope(hs,eh1s1)
+slope(hs,eh1s2)
 
 #md # If your run these lines in a notebook, you will see that
 #nb # As you can see,
-# the slopes for the $L^2$ and $H^1$ error norms are circa 3 (slightly better than the expected third and second order convergence resp. expected from theory).
+# the slopes for the $L^2$ and $H^1$ error norms are 2 and 1 for $k=1$ and 3 and 2 for $k=2$ respectively.
 #
 # Congrats, another tutorial done!

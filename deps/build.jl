a302e5d6214e9882a88169c34ffbb2f8fcb0b9ce
Oriol Colomes <oriol.colomes@gmail.com>
2021-06-15 20:42:23 +0200

    Fix in build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -16,7 +16,7 @@ files = [
   "Stokes equation" => "stokes.jl",
   "Isotropic damage model" => "isotropic_damage.jl",
   "Fluid-Structure Interaction"=>"fsi_tutorial.jl",
-  "Laminar flow around a cylinder"=>"transient_inc_navier_stokes.jl"]
+  "Laminar flow around a cylinder"=>"transient_inc_navier_stokes.jl",
   "Low-level API Poisson equation"=>"poisson_dev_fe.jl"]
 
 Sys.rm(notebooks_dir;recursive=true,force=true)

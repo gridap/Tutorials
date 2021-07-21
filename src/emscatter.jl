9df49d0553e2f190b410b7a79be995693dbb86fc
Wenjie Yao <jayyao@mit.edu>
2021-07-21 16:03:56 -0400

    update results

--- src/emscatter.jl
+++ src/emscatter.jl
@@ -3,6 +3,7 @@
 #   * How to formulate the weak form for a scalar time-harmonic electromagnetic problem
 #   * How to implement perfectly matched layers (PML)
 #   * How does periodic boundary condition work in Gridap
+#   * How to discretize PDEs with complex-valued solution
 # 
 # ## Problem statement
 # 
@@ -13,7 +14,7 @@
 # From Maxwell's equations, considering a time-harmonic electromagnetic field, we can derive the governing equation of this problem in 2D (Helmholtz equation) [1]: 
 #
 # ```math
-# \left[-\nabla\cdot\frac{1}{\varepsilon(x)}\cdot\nabla -k^2\mu(x)\right] H = f(x),
+# \left[-\nabla\cdot\frac{1}{\varepsilon(x)}\nabla -k^2\mu(x)\right] H = f(x),
 # ```
 #
 # where $k=\omega/c$ is the wave number in free space and $f(x)$ is the source term (which corresponds to a magnetic current density in Maxwell's equations).
@@ -23,29 +24,30 @@
 # There are many formulations of PML. Here, we use one of the most flexible formulations, the "stretched-coordinate" formulation, which takes the following replace in the PDE [3]:
 #
 # ```math
-# \frac{\partial}{\partial x}\rightarrow \frac{1}{1+\mathrm{i}\sigma(u)/\omega}\frac{\partial}{\partial x},
+# \frac{\partial}{\partial x}\rightarrow \frac{1}{1+\mathrm{i}\sigma(u_x)/\omega}\frac{\partial}{\partial x},
 # ```
 #
 # ```math
-# \frac{\partial}{\partial y}\rightarrow \frac{1}{1+\mathrm{i}\sigma(u)/\omega}\frac{\partial}{\partial y},
+# \frac{\partial}{\partial y}\rightarrow \frac{1}{1+\mathrm{i}\sigma(u_y)/\omega}\frac{\partial}{\partial y},
 # ```
 #
-# where $u$ is the depth into the PML, $\sigma$ is a profile function (here we chose $\sigma(u)=\sigma_0(u/d_{pml})^2$) and different derivative corresponds to different absorption directions. 
+# where $u_{x/y}$ is the depth into the PML, $\sigma$ is a profile function (here we chose $\sigma(u)=\sigma_0(u/d_{pml})^2$) and different derivative corresponds to different absorption directions. 
 # Note that at a finite mesh resolution, PML reflects some waves, and the standard technique to mitigate this is to "turn on" the PML absorption gradually—in this case we use a quadratic profile. The amplitude $\sigma_0$ is chosen so that in the limit of infinite resolution the "round-trip" normal-incidence is some small number. 
 # 
-# Since PML absorbs all waves in $x(y)$ direction, the associated boundary condition is then usually the zero Dirichlet boundary condition. Here, the boundary conditions are zero Dirichlet boundary on the top and bottom side $\Gamma_D$ but periodic boundary condition on the left ($\Gamma_L$) and right side ($\Gamma_R$). 
+# Since PML absorbs all waves in $x/y$ direction, the associated boundary condition is then usually the zero Dirichlet boundary condition. Here, the boundary conditions are zero Dirichlet boundary on the top and bottom side $\Gamma_D$ but periodic boundary condition on the left ($\Gamma_L$) and right side ($\Gamma_R$). 
 # The reason that we use a periodic boundary condition for the left and right side instead of zero Dirichlet boundary condition is that we want to simulate a plane wave exicitation, which then requires a periodic boundary condition.
 #
-# Consider $\mu(x)=1$ (which is mostly the case in electromagnetic problems) and denote $\Lambda=\frac{1}{1+\mathrm{i}\sigma(u)/\omega}$, we can formulate the problem as 
+# Consider $\mu(x)=1$ (which is mostly the case in electromagnetic problems) and denote $\Lambda=\operatorname{diagm}(\Lambda_x,\Lambda_y)$ where $\Lambda_{x/y}=\frac{1}{1+\mathrm{i}\sigma(u_{x/y})/\omega}$, we can formulate the problem as 
 #
 # ```math
 # \left\{ \begin{aligned} 
-# \left[-\Lambda\nabla\cdot\frac{1}{\varepsilon(x)}\Lambda\cdot\nabla -k^2\right] H &= f(x) & \text{ in } \Omega,\\
+# \left[-\Lambda\nabla\cdot\frac{1}{\varepsilon(x)}\Lambda\nabla -k^2\right] H &= f(x) & \text{ in } \Omega,\\
 # H&=0 & \text{ on } \Gamma_D,\\
 # H|_{\Gamma_L}&=H|_{\Gamma_R},&\\
 # \end{aligned}\right.
 # ```
-# 
+# For convenience, in the weak form and Julia implementation below we represent $\Lambda$ as a vector instead of a diagonal $2 \times 2$ matrix, in which case $\Lambda\nabla$ becomes the elementwise product.
+#
 # ## Numerical scheme
 # 
 # Similar to the previous tutorials, we need to construct the weak form for the above PDEs. After integral by part and removing the zero boundary integral term, we get:
@@ -57,6 +59,7 @@
 # ```math
 # b(v) = \int_\Omega vf\mathrm{d}\Omega
 # ```
+# Notice that the $\nabla(\Lambda v)$ is also a element-wise "product" of two vectors $\nabla$ and $\Lambda v$.
 #
 # ## Setup
 # 
@@ -75,7 +78,7 @@ xc = [0 -1.0]    # Center of the cylinder
 r = 1.0          # Radius of the cylinder
 d_pml = 0.8      # Thickness of the PML   
 k = 2*π/λ        # Wave number 
-ϵ1 = 3.0         # Relative electric permittivity for cylinder
+const ϵ₁ = 3.0   # Relative electric permittivity for cylinder
 
 # ## Discrete Model
 # 
@@ -92,8 +95,8 @@ model = GmshDiscreteModel("../models/geometry.msh")
 # ### Test and trial finite element function space
 order = 1
 reffe = ReferenceFE(lagrangian,Float64,order)
-V = TestFESpace(model,reffe,dirichlet_tags=["DirichletEdges"],vector_type=Vector{ComplexF64})
-U = TrialFESpace(V,[0])
+V = TestFESpace(model,reffe,dirichlet_tags="DirichletEdges",vector_type=Vector{ComplexF64})
+U = V # mathematically equivalent to TrialFESpace(V,0)
 
 # ## Numerical integration
 # 
@@ -159,7 +162,7 @@ const cylinder_tag = get_tag_from_name(labels,"Cylinder")
 
 function ξ(tag)
     if tag == cylinder_tag
-        return 1/ϵ1
+        return 1/ϵ₁
     else
         return 1.0
     end
@@ -169,6 +172,7 @@ end
 Λf = Λ(σ,k,LH,d_pml) 
 
 # ### Bi-linear term (from weak form)
+# Note that we use a element-wise product .* here for the vector-vector product $\Lambda \nabla$
 a(u,v) = ∫(  (∇.*(Λf*v))⊙((ξ∘τ)*(Λf.*∇(u))) - (k^2*(v*u))  )dΩ
 
 # ### Source term (uniform line source)
@@ -214,7 +218,7 @@ uh = solve(op)
 # 
 # where $n=\sqrt{\varepsilon}$ is the refractive index. 
 # 
-# Applying the boundary conditions (tangential part of the electri and magnetic field to be continuous): 
+# Applying the boundary conditions (tangential part of the electric and magnetic field to be continuous): 
 #
 # ```math
 # H_{inc}+H_1=H_2|_{r=R},
@@ -249,7 +253,7 @@ uh = solve(op)
 # ```
 #
 # ```math
-# H(r<=R)=\frac{1}{2k}\sum_m\beta_mi^mJ_m(nkr)e^{im\theta}
+# H(r\leq R)=\frac{1}{2k}\sum_m\beta_mi^mJ_m(nkr)e^{im\theta}
 # ```
 # 
 
@@ -279,15 +283,11 @@ function H_t(x,xc,r,ϵ,λ)
 end
 #
 # ### Construct the analytical solution in finite element basis
-# Basically we are solving a equation $u=H_t$ which has a weak form $a_t(u,v)=uv$ and source term $b_t(v)=H_t(x)$
-a_t(u,v) = ∫(u*v)*dΩ
-b_t(v) = ∫(v*(x->H_t(x,xc,r,ϵ1,λ)))*dΩ
-op_t = AffineFEOperator(a_t,b_t,U,V)
-uh_t = solve(op_t)
+uh_t = CellField(x->H_t(x,xc,r,ϵ₁,λ),Ω)
 
 # ## Output and compare results
 # 
-# The simulated field is shown below. We can see that the simulated fields and the analytical solution matched closed except for the top and PML regions. This is because the simulated source generate plane waves in two directions but we only consider the downward propagating wave in the analytical solution and the PML effect is also not considered in the analytical solution. Therefore, we just need to focus on the "center" regions which excludes the PML and top region above the source, the difference is within 2% of the field amplitude integral. As we increase the resolution, this difference should decrease (until it becomes limited by the PML reflection coefficient from $\sigma_0$, the number of Bessel function basis $M$ or by floating-point error.)
+# The simulated field is shown below. We can see that the simulated fields and the analytical solution matched closed except for the top and PML regions. This is because the simulated source generate plane waves in two directions but we only consider the downward propagating wave in the analytical solution and the PML effect is also not considered in the analytical solution. Therefore, we just need to focus on the "center" regions which excludes the PML and top region above the source, the difference is within 6% of the field amplitude integral. As we increase the resolution, this difference should decrease (until it becomes limited by the PML reflection coefficient from $\sigma_0$, the number of Bessel function basis $M$ or by floating-point error.)
 # ![](../assets/emscatter/Results.png)
 
 # ### Save to file and view
@@ -297,7 +297,7 @@ writevtk(Ω,"demo",cellfields=["Real"=>real(uh),
         "Real_t"=>real(uh_t),
         "Imag_t"=>imag(uh_t),
         "Norm_t"=>abs2(uh_t),
-        "Difference"=>abs2(uh_t-uh)])
+        "Difference"=>abs(uh_t-uh)])
 # ### Compare the difference in the "center" region
 function AnalyticalBox(x) # Get the "center" region
     if abs(x[1])<L/2 && abs(x[2]+0.5)<2.5
@@ -307,7 +307,9 @@ function AnalyticalBox(x) # Get the "center" region
     end
 end
 
-Difference=sum(∫(abs2(uh_t-uh)*AnalyticalBox)*dΩ)/sum(∫(abs2(uh)*AnalyticalBox)*dΩ)
+Difference=sqrt(sum(∫(abs2(uh_t-uh)*AnalyticalBox)*dΩ)/sum(∫(abs2(uh_t)*AnalyticalBox)*dΩ))
+
+@assert Difference < 0.1
 #
 # ## References
 # [1] https://en.wikipedia.org/wiki/Electromagnetic_wave_equation
